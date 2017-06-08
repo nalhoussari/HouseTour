6d553340646ca2bd375c277219285299f8b090f0
nalhoussari <nalhoussari@hotmail.com>
2017-06-07 19:00:06 -0700

    Submitting HouseTour assignment

---
+++ HouseTour/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HouseTour
+//
+//  Created by Noor Alhoussari on 2017-06-07.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HouseTour/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HouseTour
+//
+//  Created by Noor Alhoussari on 2017-06-07.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
